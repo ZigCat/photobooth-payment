bd42b266c1f4597f2c95cfff877c80e88a1da0e8
ZigCat <zigcat517@gmail.com>
2025-06-06 22:59:39 +0500

    config price

--- renderer/config/config.js
+++ renderer/config/config.js
@@ -3,11 +3,11 @@ export default{
   proxyApiKey: process.env.PROXY_API_KEY,
   dslrBaseUrl: process.env.DSLR_BASE_URL,
   dslrPassword: process.env.DSLR_PASSWORD,
+  copyPrice: process.env.COPY_PRICE,
+  sessionPrice: process.env.SESSION_PRICE,
   pollingInterval: 2000,
   maxPollingAttempts: 5,
   preSessionDelay: 5000,
   sessionDuration: 5000,
   printDuration: 5000,
-  copyPrice: 2,
-  sessionPrice: 2
 };
